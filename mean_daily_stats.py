3673c6ac8b389d84adead17ac1468d70981fee20
Kavi Gupta <kavi@berkeley.edu>
2025-09-15 17:25:49 -0400

    add more temperature analyses

--- mean_daily_stats.py
+++ mean_daily_stats.py
@@ -1,5 +1,5 @@
 import ee
-from datetime import datetime
+from datetime import datetime, timedelta
 from permacache import permacache
 
 from constants import date_start_str, date_end_str
@@ -14,7 +14,10 @@ from download import download_ee_image
 
 
 @permacache("weather-agg-ee/mean_daily_stats/mean_daily_stats_for_segment")
-def mean_daily_stats_for_segment(band, filter_spec):
+def mean_daily_stats_for_segment_and_timespan(
+    band, filter_spec, date_start_str, date_end_str
+):
+    print(band, filter_spec, date_start_str, date_end_str)
     ee.Initialize()
     era5 = ee.ImageCollection("ECMWF/ERA5/DAILY")
     data = era5.filter(ee.Filter.date(ee.Date(date_start_str), ee.Date(date_end_str)))
@@ -27,8 +30,30 @@ def mean_daily_stats_for_segment(band, filter_spec):
     mean_temp_for_segment = data.mean()
 
     return download_ee_image(
-        mean_temp_for_segment, band, resolution=0.25, degree_size=60
+        mean_temp_for_segment, band, resolution=0.25, degree_size=45
     )
+
+def decrement(date_str):
+    date = datetime.strptime(date_str, "%Y-%m-%d")
+    date -= timedelta(days=1)
+    return date.strftime("%Y-%m-%d")
+
+def mean_daily_stats_for_segment(band, filter_spec):
+    dates = ["1990-01-01", "2000-01-01", "2010-01-01", "2020-01-01"]
+    assert dates[0] == date_start_str and decrement(dates[-1]) == date_end_str
+    results = 0
+    total_weight = 0
+    for start, end in zip(dates[:-1], dates[1:]):
+        result = mean_daily_stats_for_segment_and_timespan(
+            band, filter_spec, start, end
+        )
+        weight = (
+            datetime.strptime(end, "%Y-%m-%d") - datetime.strptime(start, "%Y-%m-%d")
+        ).days
+        results += result * weight
+        total_weight += weight
+
+    return results / total_weight
 
 
 def for_breaks(band, year_zero, breaks):
@@ -71,6 +96,7 @@ def month_seasonal_summary(band):
         datetime(2021, 6, 1),
         datetime(2021, 9, 1),
         datetime(2021, 12, 1),
+        datetime(2021, 12, 31),
     ]
     return for_breaks(band, year_zero, month_breaks)
 
