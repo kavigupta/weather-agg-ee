3ef9947f9ce6b911dc78ad6dc51cd8033bedeb79
Kavi Gupta <kavi@berkeley.edu>
2025-09-22 22:23:34 -0400

    update

--- dewpoint.py
+++ dewpoint.py
@@ -1,23 +1,32 @@
+import multiprocessing
 from datetime import datetime, timedelta
 
 import ee
 from permacache import permacache
 
 from download import download_ee_image
-from sample import sampled_values
+from sample import compute_date_strs
 
 
 @permacache("weather-agg-ee/dewpoint/high_dewpoint_for_date", multiprocess_safe=True)
 def high_dewpoint_for_date(date_str):
+    start = datetime.now()
+    print(f"{start} - Start {date_str}")
     ee.Initialize()
     date = ee.Date(date_str)
     era5_land = ee.ImageCollection("ECMWF/ERA5_LAND/HOURLY")
 
     day_collection = era5_land.filter(ee.Filter.date(date, date.advance(1, "day")))
 
-    return download_ee_image(
-        day_collection.max(), "dewpoint_temperature_2m", resolution=0.25, degree_size=60
+    result = download_ee_image(
+        day_collection.max(),
+        "dewpoint_temperature_2m",
+        resolution=0.25,
+        degree_size=60,
+        pbar=False,
     )
+    end = datetime.now()
+    print(f"{end} - Finished {date_str}; took {end - start}")
 
     # results = []
     # for i in range(0, num_samples, chunk_size):
@@ -29,6 +38,14 @@ def high_dewpoint_for_date(date_str):
     # return np.array(results)
 
 
+def high_dewpoint_for_date_for_parallel(date_str):
+    return high_dewpoint_for_date(date_str)
+
+
+def populate_caches():
+    with multiprocessing.Pool(processes=8) as pool:
+        pool.map(high_dewpoint_for_date_for_parallel, compute_date_strs())
+
+
 if __name__ == "__main__":
-    for _ in sampled_values(high_dewpoint_for_date, 2000, quiet=False):
-        pass
+    populate_caches()
--- sample.py
+++ sample.py
@@ -7,15 +7,24 @@ from constants import date_end_str, date_start_str
 
 
 def sampled_values(fn, num_samples, quiet=True):
+    date_strs = compute_date_strs()
+
+    for i, date_str in enumerate(tqdm.tqdm(date_strs)):
+        if not quiet:
+            print(f"Processing date #{i} of {num_samples}: {date_str}")
+        yield fn(date_str)
+
+
+def compute_date_strs():
     start_date = datetime.strptime(date_start_str, "%Y-%m-%d").date()
     num_dates = (
         datetime.strptime(date_end_str, "%Y-%m-%d").date() - start_date
     ).days + 1
     shuffled_dates = np.random.RandomState(0).permutation(num_dates)
 
-    for i in tqdm.trange(num_samples):
-        date = start_date + timedelta(days=int(shuffled_dates[i]))
-        date_str = date.strftime("%Y-%m-%d")
-        if not quiet:
-            print(f"Processing date #{i} of {num_samples}: {date_str}")
-        yield fn(date_str)
+    date_strs = [
+        (start_date + timedelta(days=int(shuffled_dates[i]))).strftime("%Y-%m-%d")
+        for i in range(num_dates)
+    ]
+
+    return date_strs
